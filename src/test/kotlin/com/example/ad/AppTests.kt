ed85714b56ca23358e8d3ba29d6f26bf794f3aa8
solomon.maeng <solomon.maeng@dramancompany.com>
2023-09-28 17:04:17 +0900

    프로젝트 설정
    - Kotlin Spring 프로젝트 설정했습니다.
    - CI에서 Lint와 Test가 가능하도록 Workflow를 추가했습니다.

---
+++ src/test/kotlin/com/example/ad/AppTests.kt
@@ -0,0 +1,16 @@
+package com.example.ad
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppTests {
+
+    @Test
+    fun `CI 테스트`() {
+        val a = 1
+        assertThat(a).isEqualTo(1)
+    }
+
+}
