030b852c56bdd9a6cd66f300da2ca0e61c3f35f0
solomon.maeng <solomon.maeng@dramancompany.com>
2023-10-06 20:44:08 +0900

    캠페인 생성 시 입력 값이 공백인 경우를 검증하는 테스트 추가

--- src/main/kotlin/com/example/ad/contract/CreateCampaignApi.kt
+++ src/main/kotlin/com/example/ad/contract/CreateCampaignApi.kt
@@ -1,10 +1,13 @@
 package com.example.ad.contract
 
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotBlank
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.ProblemDetail
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -14,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController
 class CreateCampaignApi {
 
     @PostMapping("/api/v1/campaigns/contract")
-    fun create(@RequestBody request: CreateCampaignRequest) {
+    fun create(@RequestBody @Valid request: CreateCampaignRequest) {
     }
 
     @ExceptionHandler(HttpMessageNotReadableException::class)
@@ -22,6 +25,13 @@ class CreateCampaignApi {
         return ResponseEntity
             .badRequest()
             .body(ProblemDetails.forNullInput())
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handle(e: MethodArgumentNotValidException): ResponseEntity<ProblemDetail> {
+        return ResponseEntity
+            .badRequest()
+            .body(ProblemDetails.forEmptyInput())
     }
 
     companion object {
@@ -36,12 +46,27 @@ object ProblemDetails {
             ErrorMessage.NULL_INPUT.value,
         )
     }
+
+    fun forEmptyInput(): ProblemDetail {
+        return ProblemDetail.forStatusAndDetail(
+            HttpStatusCode.valueOf(HttpStatus.BAD_REQUEST.value()),
+            ErrorMessage.EMPTY_INPUT.value,
+        )
+    }
 }
 
 enum class ErrorMessage(val value: String) {
     NULL_INPUT("해당 입력 값은 NULL일 수 없습니다"),
+    EMPTY_INPUT("해당 입력 값은 공백일 수 없습니다"),
 }
 
 data class CreateCampaignRequest(
+    @field:NotBlank
+    val clientId: String,
+    @field:NotBlank
     val name: String,
+    @field:NotBlank
+    val createdBy: String,
+    @field:NotBlank
+    val campaignType: String,
 )
