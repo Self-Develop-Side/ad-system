ebc7ff930b22d796097125f07f64316232f1c9f5
solomon.maeng <solomon.maeng@dramancompany.com>
2023-10-06 20:44:08 +0900

    캠페인 생성 시 입력 값이 공백인 경우를 검증하는 테스트 추가

--- src/test/kotlin/com/example/ad/contract/CreateCampaignApiTests.kt
+++ src/test/kotlin/com/example/ad/contract/CreateCampaignApiTests.kt
@@ -3,14 +3,13 @@ package com.example.ad.contract
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EmptySource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.server.LocalServerPort
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
+import org.springframework.http.*
 import org.springframework.test.context.ActiveProfiles
 import java.net.URI
 
@@ -33,17 +32,34 @@ class CreateCampaignApiTests {
         val request: HttpEntity<String> = HttpEntity(null, headers)
 
         val response = client.postForEntity(
-            "http://localhost:${port}${CreateCampaignApi.PATH}",
-            request,
-            String::class.java,
+            requestURI(), request, String::class.java,
         )
 
-        val errorResult = objectMapper.writeValueAsString(
-            ProblemDetails.forNullInput().apply {
+        assertThat(response.statusCode.value()).isEqualTo(HttpStatus.BAD_REQUEST.value())
+        assertThat(response.body).isEqualTo(expectedProblemDetail(ProblemDetails.forNullInput()))
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    fun `캠페인 생성에 필요한 입력 값이 공백인 경우 400 에러가 발생한다`(value: String) {
+        val request = CreateCampaignRequest(value, value, value, value)
+
+        val response = client.postForEntity(
+            requestURI(), request, String::class.java,
+        )
+
+        assertThat(response.statusCode.value()).isEqualTo(HttpStatus.BAD_REQUEST.value())
+        assertThat(response.body).isEqualTo(expectedProblemDetail(ProblemDetails.forEmptyInput()))
+    }
+
+    private fun requestURI() = "http://localhost:${port}${CreateCampaignApi.PATH}"
+
+    private fun expectedProblemDetail(problemDetail: ProblemDetail): String {
+        val expected = objectMapper.writeValueAsString(
+            problemDetail.apply {
                 instance = URI(CreateCampaignApi.PATH)
             },
         )
-        assertThat(response.statusCode.value()).isEqualTo(HttpStatus.BAD_REQUEST.value())
-        assertThat(response.body).isEqualTo(errorResult)
+        return expected
     }
 }
