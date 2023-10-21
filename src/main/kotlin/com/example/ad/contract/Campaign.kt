29fa74d21e8b18ef75b2765a8636384296a69ca0
solomon.maeng <solomon.maeng@dramancompany.com>
2023-10-21 16:02:57 +0900

    캠페인 생성 시 클라이언트 식별자가 올바르지 않은 경우에 대한 검증 테스트 작성

---
+++ src/main/kotlin/com/example/ad/contract/Campaign.kt
@@ -0,0 +1,11 @@
+package com.example.ad.contract
+
+import java.util.*
+
+class Campaign(
+    val id: UUID,
+    val clientId: ClientId,
+    val name: String,
+    val createdBy: String,
+    val campaignType: CampaignType,
+)
--- src/main/kotlin/com/example/ad/contract/ValidationErrors.kt
+++ src/main/kotlin/com/example/ad/contract/ValidationErrors.kt
@@ -7,4 +7,11 @@ object ValidationErrors {
             "캠페인 타입이 올바르지 않습니다",
         )
     }
+
+    fun invalidClientId(): ValidationError {
+        return ValidationError(
+            "clientId",
+            "클라이언트 아이디가 올바르지 않습니다",
+        )
+    }
 }
